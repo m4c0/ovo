1ef852347b760b50f1f5b867a21f5e861c4a0605
Eduardo Costa <ecosta@hubspot.com>
2023-02-11 17:46:59 +0000

    skething the module interface

--- build.cpp
+++ build.cpp
@@ -2,6 +2,8 @@
 #include "ecow.hpp"
 
 class cmake : public ecow::unit {
+  pathset m_objs{};
+
   void build_self() const override {
     namespace fs = std::filesystem;
 
@@ -21,23 +23,49 @@ class cmake : public ecow::unit {
       throw std::runtime_error("Failed to build with CMake");
   }
 
-  [[nodiscard]] pathset self_objects() const override { return {}; }
+  [[nodiscard]] pathset self_objects() const override {
+    namespace fs = std::filesystem;
+    const auto out = fs::path{ecow::impl::current_target()->build_folder()};
+    const auto install = fs::absolute(out / "install");
+
+    pathset res{};
+    for (const auto &o : m_objs) {
+      res.insert(install / o);
+    }
+    return res;
+  }
 
 public:
   using unit::unit;
+
+  void add_object(const std::string &o) { m_objs.insert(o); }
 };
 
 int main(int argc, char **argv) {
   using namespace ecow;
 
-  auto m = unit::create<seq>("ovo");
+  auto m = unit::create<seq>("ovo-all");
   m->add_wsdep("yoyo", yoyo());
-  m->add_unit<cmake>("ogg");
-  m->add_unit<cmake>("vorbis");
-  m->add_unit<mod>("ovo");
+  m->add_unit<cmake>("ogg")->add_object("lib/libogg.a");
+
+  auto v = m->add_unit<cmake>("vorbis");
+  v->add_object("lib/libvorbis.a");
+  v->add_object("lib/libvorbisfile.a");
+
+  auto mm = m->add_unit<mod>("ovo");
+  mm->add_include_dir("ogg/include");
+  mm->add_include_dir("vorbis/include");
 
   auto poc = unit::create<exe>("poc");
+  poc->add_wsdep("yoyo", yoyo_libc());
   poc->add_ref(m);
   poc->add_unit("poc");
-  return ecow::run_main(poc, argc, argv);
+
+  auto all = unit::create<per_feat<seq>>("all");
+  all->for_feature(windows_api).add_ref(poc);
+  all->for_feature(cocoa).add_ref(poc);
+  all->for_feature(android_ndk).add_ref(m);
+  all->for_feature(webassembly).add_ref(m);
+
+  return ecow::run_main(all, argc, argv);
 }
--- poc.cpp
+++ poc.cpp
@@ -1,3 +1,14 @@
+import yoyo_libc;
 import ovo;
 
-int main() {}
+int main() {
+  yoyo::file_reader r{"silent.ogg"};
+  ovo::decoder d{&r};
+
+  while (d.preload()) {
+  }
+
+  char buf[1024];
+  while (d.fill(buf, sizeof(buf)) > 0) {
+  }
+}
