ca22ca795117f92454b79a62990360143d053e9b
Eduardo Costa <m4c0@users.noreply.github.com>
2023-02-13 14:14:17 +0000

    using plain c instead of cmake
    who needs another build system? :P

--- build.cpp
+++ build.cpp
@@ -1,63 +1,15 @@
 #include "../yoyo/build.hpp"
 #include "ecow.hpp"
 
-class cmake : public ecow::unit {
-  pathset m_objs{};
-
-  void build_self() const override {
-    namespace fs = std::filesystem;
-
-    const auto out = fs::path{ecow::impl::current_target()->build_folder()};
-    const auto build = fs::absolute(out / name() / "build").string();
-    const auto install = fs::absolute(out / "install").string();
-    const auto flags = ecow::impl::current_target()->cxxflags();
-
-    auto setup = std::string{"cmake -S "} + name() + " -B " + build +
-                 " -DCMAKE_BUILD_TYPE=Release " +
-                 " -DCMAKE_FIND_ROOT_PATH=" + install +
-                 " -DCMAKE_INSTALL_PREFIX=" + install + " -DCMAKE_C_FLAGS=\"" +
-                 flags + "\"";
-    if (std::system(setup.c_str()) != 0)
-      throw std::runtime_error("Failed to setup CMake");
-
-    auto compile = std::string{"cmake --build "} + build + " --target install";
-    if (std::system(compile.c_str()) != 0)
-      throw std::runtime_error("Failed to build with CMake");
-  }
-
-  [[nodiscard]] pathset self_objects() const override {
-    namespace fs = std::filesystem;
-    const auto out = fs::path{ecow::impl::current_target()->build_folder()};
-    const auto install = fs::absolute(out / "install");
-
-    pathset res{};
-    for (const auto &o : m_objs) {
-      res.insert(install / o);
-    }
-    return res;
-  }
-
-public:
-  using unit::unit;
-
-  void add_object(const std::string &o) { m_objs.insert(o); }
-};
-
 int main(int argc, char **argv) {
   using namespace ecow;
 
   auto m = unit::create<seq>("ovo-all");
   m->add_wsdep("yoyo", yoyo());
-  m->add_unit<cmake>("ogg")->add_object("lib/libogg.a");
 
-  auto v = m->add_unit<cmake>("vorbis");
-#ifdef _WIN32
-  v->add_object("lib/vorbis.lib");
-  v->add_object("lib/vorbisfile.lib");
-#else
-  v->add_object("lib/libvorbis.a");
-  v->add_object("lib/libvorbisfile.a");
-#endif
+  auto ov = m->add_unit<>("oggvorbis.c");
+  ov->add_include_dir("ogg/include");
+  ov->add_include_dir("vorbis/include");
 
   auto mm = m->add_unit<mod>("ovo");
   mm->add_include_dir("ogg/include");
