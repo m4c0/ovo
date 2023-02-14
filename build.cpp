7c15da878dc3fcb8cd453fe4b504f50e8cdbe021
Eduardo Costa <ecosta@hubspot.com>
2023-02-14 16:10:50 +0000

    split build header

--- build.cpp
+++ build.cpp
@@ -1,19 +1,9 @@
-#include "../yoyo/build.hpp"
-#include "ecow.hpp"
+#include "build.hpp"
 
 int main(int argc, char **argv) {
   using namespace ecow;
 
-  auto m = unit::create<seq>("ovo-all");
-  m->add_wsdep("yoyo", yoyo());
-
-  auto ov = m->add_unit<>("oggvorbis.c");
-  ov->add_include_dir("ogg/include");
-  ov->add_include_dir("vorbis/include");
-
-  auto mm = m->add_unit<mod>("ovo");
-  mm->add_include_dir("ogg/include");
-  mm->add_include_dir("vorbis/include");
+  auto m = ovo();
 
   auto poc = unit::create<exe>("poc");
   poc->add_wsdep("yoyo", yoyo_libc());
