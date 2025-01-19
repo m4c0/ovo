1b5ee6521da28bb3485f001f6c8c9a2894d6dd75
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-19 14:53:26 +0000

    improving output of poc

--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,6 @@
 #pragma leco tool
 
-import silog;
+import print;
 import yoyo;
 import ovo;
 
@@ -9,15 +9,17 @@ void run(yoyo::reader * r) {
 
   while (d.preload()) {
   }
+  putln("preloaded");
 
   constexpr const auto buf_size = 1024;
   float buf[buf_size];
   while (d.fill(buf, buf_size) > 0) {
   }
+  putln("all chunks 'played'");
 }
 
 int main() {
   yoyo::file_reader::open("silent.ogg")
     .map([](auto & r) { run(&r); })
-    .take([](auto msg) { silog::die("failure"); });
+    .take([](auto msg) { die("failure: ", msg); });
 }
